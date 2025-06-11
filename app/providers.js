af36a0f6d9b2666c17eec652c922986a3d52b12e
Khoury Smith <khoury.smith@gmail.com>
2025-06-11 15:45:32 -0400

    ready his toime

--- app/providers.js
+++ app/providers.js
@@ -2,7 +2,7 @@
 "use client";
 
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "@/styles/theme";
+import { theme } from "../src/styles/theme";
 
 export function Providers({ children }) {
   return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
